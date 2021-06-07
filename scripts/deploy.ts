846c21433bab88b7be55eae84d099af112223e28
SidneyNemzer <SidneyNemzer@users.noreply.github.com>
2021-06-06 17:53:46 -0700

    fix deploy script
    was copying build folder to the wrong location, windows paths seemed to be confusing scp

--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -4,7 +4,27 @@ import * as path from "path";
 
 dotenv.config({ path: ".env.deploy" });
 
-const PROJECT_ROOT = path.resolve(__dirname, "..");
+/**
+ * Converts a Windows path to Posix. If the path is already Posix, the path
+ * is returned unchanged.
+ */
+const toPosixPath = (input: string) => {
+  if (input.includes("/")) {
+    return input;
+  }
+
+  const parts = input.split("\\");
+
+  if (parts[0] && parts[0].includes(":")) {
+    parts[0] = parts[0].replace(":", "").toLowerCase();
+    // Ensure returned path starts with a slash
+    parts.unshift("");
+  }
+
+  return parts.join("/");
+};
+
+const PROJECT_ROOT = toPosixPath(path.resolve(__dirname, ".."));
 const BUILD_DIRECTORY = "build";
 
 const EXPECTED_ENV = ["SERVER_IP", "SERVER_USERNAME", "SERVER_DIRECTORY"];
@@ -27,24 +47,38 @@ const main = async () => {
   // TODO remove old files
   copyFile("package.json");
   copyFile("package-lock.json");
-  copyDirectory("build");
+  copyFile("pm2.yaml");
+  copyDirectory(BUILD_DIRECTORY);
   // TODO npm i --production
   // TODO restart server
   //  NODE_ENV=production PORT=80 node ./build
 };
 
 const copyFile = (...parts: string[]) => {
-  const source = path.resolve(PROJECT_ROOT, ...parts);
-  const destination = `${process.env.SERVER_DIRECTORY}${parts.join("/")}`;
-  const destinationWithLogin = `${process.env.SERVER_USERNAME}@${process.env.SERVER_IP}:${destination}`;
-  execSync("scp", [source, destinationWithLogin]);
+  const source = path.posix.resolve(PROJECT_ROOT, ...parts);
+  const destination = [
+    process.env.SERVER_USERNAME,
+    "@",
+    process.env.SERVER_IP,
+    ":",
+    process.env.SERVER_DIRECTORY,
+    parts.join("/"),
+  ].join("");
+  execSync("scp", [source, destination]);
 };
 
 const copyDirectory = (...parts: string[]) => {
-  const source = path.resolve(PROJECT_ROOT, ...parts);
-  const destination = `${process.env.SERVER_DIRECTORY}${parts.join("/")}`;
-  const destinationWithLogin = `${process.env.SERVER_USERNAME}@${process.env.SERVER_IP}:${destination}`;
-  execSync("scp", ["-r", source, destinationWithLogin]);
+  // TODO this will not handle subdirectories like `stuff/things`. It will be
+  // created at `/root/helium3/things` instead of `/root/helium3/stuff/things`.
+  const source = path.posix.resolve(PROJECT_ROOT, ...parts);
+  const destination = [
+    process.env.SERVER_USERNAME,
+    "@",
+    process.env.SERVER_IP,
+    ":",
+    process.env.SERVER_DIRECTORY,
+  ].join("");
+  execSync("scp", ["-r", source, destination]);
 };
 
 const execSync = (command: string, args: string[]) => {
