89b7c6c2c73eef32c600b2c3c243273c644791fa
Sidney Nemzer <SidneyNemzer@users.noreply.github.com>
2024-01-21 09:54:41 -0500

    fix assets in run-win.sh

--- scripts/run-win.sh
+++ scripts/run-win.sh
@@ -1,9 +1,17 @@
-#!/bin/sh
+#!/bin/bash
+
+set -euo pipefail
 
 # Builds for Windows and runs the executable. Native linux binary has much worse
 # performance in WSL2 than the Windows binary.
 
 NAME=$1
 
-cargo build --example $NAME --target x86_64-pc-windows-gnu &&
+cargo build --example $NAME --target x86_64-pc-windows-gnu
+
+# Set CARGO_MANIFEST_DIR so that the Windows binary can find the assets
+# directory.
+export CARGO_MANIFEST_DIR="/home/sidney/code/helium3-rust"
+export WSLENV=CARGO_MANIFEST_DIR
+
 exec target/x86_64-pc-windows-gnu/debug/examples/$NAME.exe
